2c365d958addf18b1fd8b64f4ffae1f12193373a
Koichi Akaike <k-akaike@nttpc.co.jp>
2017-12-11 14:13:16 +0000

    blogs commit 1st

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root 'tops#index'
+  #root 'blogs#index'
+  
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
